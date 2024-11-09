98a09df873790eb955f001f2b20a05b08bed941c
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2024-11-09 15:25:12 +0100

    (Temp fix) Update physics material bindings
    This is a temporary non functional fix needed for the solution to compile.

--- Jolt/Bindings/Bindings_JPH_PhysicsMaterial.cs
+++ Jolt/Bindings/Bindings_JPH_PhysicsMaterial.cs
@@ -4,7 +4,7 @@
     {
         public static NativeHandle<JPH_PhysicsMaterial> JPH_PhysicsMaterial_Create()
         {
-            return CreateHandle(UnsafeBindings.JPH_PhysicsMaterial_Create());
+            return CreateHandle(UnsafeBindings.JPH_PhysicsMaterial_Create(null, 0));
         }
         
         public static void JPH_PhysicsMaterial_Destroy(NativeHandle<JPH_PhysicsMaterial> material)
