d3f5ffd51a5117bb9adf3716feea39be5879245a
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2024-11-20 19:12:25 +0100

    Add Default method to ExtendedUpdateSettings

--- Jolt/Physics/Character/ExtendedUpdateSettings.cs
+++ Jolt/Physics/Character/ExtendedUpdateSettings.cs
@@ -17,5 +17,23 @@ namespace Jolt
         public float walkStairsCosAngleForwardContact;
 
         public float3 walkStairsStepDownExtra;
+
+        /// <summary>
+        /// Returns a new instance of <see cref="ExtendedUpdateSettings"/> initialized with default values defined here :
+        /// https://jrouwe.github.io/JoltPhysics/struct_character_virtual_1_1_extended_update_settings.html
+        /// </summary>
+        /// <returns></returns>
+        public static ExtendedUpdateSettings Default()
+        {
+            return new ExtendedUpdateSettings
+            {
+                stickToFloorStepDown = new float3(0.0f, 0.5f, 0.0f),
+                walkStairsStepUp = new float3(0.0f, 0.4f, 0.0f),
+                walkStairsMinStepForward = 0.02f,
+                walkStairsStepForwardTest = 0.15f,
+                walkStairsCosAngleForwardContact = math.cos(math.radians(75.0f)),
+                walkStairsStepDownExtra = float3.zero,
+            };
+        }
     }
 }
