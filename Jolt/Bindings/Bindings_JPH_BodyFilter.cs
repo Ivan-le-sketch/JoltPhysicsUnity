f8a4838e9ef7b2931089d85f5c50325296433b16
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2024-12-01 17:29:39 +0100

    Implement managed body filter

--- Jolt/Bindings/Bindings_JPH_BodyFilter.cs
+++ Jolt/Bindings/Bindings_JPH_BodyFilter.cs
@@ -1,22 +1,15 @@
-﻿using System;
-
-namespace Jolt
+﻿namespace Jolt
 {
     internal static unsafe partial class Bindings
     {
-        public static void JPH_BodyFilter_SetProcs(NativeHandle<JPH_BodyFilter> filter)
+        public static NativeHandle<JPH_BodyFilter> JPH_BodyFilter_Create(JPH_BodyFilter_Procs procs)
         {
-            throw new NotImplementedException();
-        }
-
-        public static NativeHandle<JPH_BodyFilter> JPH_BodyFilter_Create()
-        {
-            throw new NotImplementedException();
+            return CreateHandle(UnsafeBindings.JPH_BodyFilter_Create(procs, null));
         }
 
         public static void JPH_BodyFilter_Destroy(NativeHandle<JPH_BodyFilter> filter)
         {
-            throw new NotImplementedException();
+            UnsafeBindings.JPH_BodyFilter_Destroy(filter);
         }
     }
 }
---
+++ Jolt/Physics/Collision/BodyFilter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Jolt
+{
+    /// <summary>
+    /// Filter defining bodies to ignore in a collision query.
+    /// </summary>
+    public unsafe class BodyFilter : IDisposable
+    {
+        internal NativeHandle<JPH_BodyFilter> Handle;
+
+        /// <summary>
+        /// HashSet determining body ids to ignore.
+        /// </summary>
+        private HashSet<BodyID> ignoredBodyIDs = new HashSet<BodyID>();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate bool ShouldCollide(IntPtr context, BodyID bodyID);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate bool ShouldCollideLocked(IntPtr context, JPH_Body* body);
+
+        // Keep a reference to the delegate to prevent garbage collection
+        internal ShouldCollide shouldCollideDelegate;
+        // Keep a reference to the delegate to prevent garbage collection
+        internal ShouldCollideLocked shouldCollideLockedDelegate;
+
+        public BodyFilter()
+        {
+            CreateNativeHandle();
+        }
+
+        public BodyFilter(List<BodyID> bodyIDs)
+        {
+            CreateNativeHandle();
+
+            foreach (BodyID bodyID in bodyIDs)
+            {
+                AddIgnoredBodyID(bodyID);
+            }
+        }
+
+        public void Dispose()
+        {
+            Bindings.JPH_BodyFilter_Destroy(Handle);
+        }
+
+        public void AddIgnoredBodyID(BodyID bodyID)
+        {
+            ignoredBodyIDs.Add(bodyID);
+        }
+
+        public void RemoveIgnoredBodyID(BodyID bodyID)
+        {
+            ignoredBodyIDs.Remove(bodyID);
+        }
+
+        public void ClearIgnoredBodyID()
+        {
+            ignoredBodyIDs.Clear();
+        }
+
+        internal bool ShouldCollideCallback(IntPtr context, BodyID bodyID)
+        {
+            return !ignoredBodyIDs.Contains(bodyID);
+        }
+
+        internal bool ShouldCollideLockedCallback(IntPtr context, JPH_Body* body)
+        {
+            var bodyHandle = new Body(new NativeHandle<JPH_Body>(body));
+
+            bool result = !ignoredBodyIDs.Contains(bodyHandle.GetID());
+
+            bodyHandle.Handle.Dispose();
+
+            return result;
+        }
+
+        private void CreateNativeHandle()
+        {
+            shouldCollideDelegate = ShouldCollideCallback;
+            shouldCollideLockedDelegate = ShouldCollideLockedCallback;
+
+            var procs = new JPH_BodyFilter_Procs
+            {
+                ShouldCollide = Marshal.GetFunctionPointerForDelegate(shouldCollideDelegate),
+                ShouldCollideLocked = Marshal.GetFunctionPointerForDelegate(shouldCollideLockedDelegate),
+            };
+
+            Handle = Bindings.JPH_BodyFilter_Create(procs);
+        }
+    }
+}
