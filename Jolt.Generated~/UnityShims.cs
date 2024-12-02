6745b2474edbdd5848ac0b57ea973995c77d1d50
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2024-12-02 11:15:55 +0100

    Update UnityShims.cs

--- Jolt.Generated~/UnityShims.cs
+++ Jolt.Generated~/UnityShims.cs
@@ -10,13 +10,24 @@ namespace Unity.Mathematics
         public float2(float x, float y) { } // used by Bindings_JPH_HingeConstraint
     }
 
-    public struct float3 { }
+    public struct float3
+    {
+        public static float3 zero { get; } // used by ExtendedUpdateSettings
+
+        public float3(float x, float y, float z) { } // used by ExtendedUpdateSettings
+    }
 
     public struct float4 { }
 
     public struct float4x4 { }
 
     public struct quaternion { }
+
+    public static class math
+    {
+        public static float radians(float angle) => angle; // used by ExtendedUpdateSettings
+        public static float cos(float value) => value; // used by ExtendedUpdateSettings
+    }
 }
 
 namespace UnityEngine
@@ -33,6 +44,19 @@ namespace UnityEngine
 
 namespace Unity.Collections
 {
+    public struct NativeList<T> : System.IDisposable
+    {
+        public NativeList(int _, Allocator __) { }
+
+        public bool IsCreated => true;
+
+        public void Add(T _) { }
+
+        public bool Contains(T _) => true;
+
+        public void Dispose() { }
+    }
+
     public struct NativeHashSet<T> : System.IDisposable
     {
         public NativeHashSet(int _, Allocator __) { }
