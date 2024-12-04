1175ec0b0ffbf33951902f079e03ee403c6d4328
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2024-12-04 12:16:34 +0100

    Add StateRecorderFilter bindings

---
+++ Jolt/Bindings/Bindings_JPH_StateRecorderFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Jolt
+{
+    internal static unsafe partial class Bindings
+    {
+        public static NativeHandle<JPH_StateRecorderFilter> JPH_StateRecorderFilter_Create(JPH_StateRecorderFilter_Procs procs, IntPtr userData)
+        {
+            return CreateHandle(UnsafeBindings.JPH_StateRecorderFilter_Create(procs, (void*) userData));
+        }
+
+        public static void JPH_StateRecorderFilter_Destroy(NativeHandle<JPH_StateRecorderFilter> filter)
+        {
+            UnsafeBindings.JPH_StateRecorderFilter_Destroy(filter);
+
+            filter.Dispose();
+        }
+    }
+}
