284529d69c974a3802f267c35f5b13f29cf41f5f
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2025-01-01 23:35:46 +0100

    Replace generic collision collector by explicit collision collectors

--- Jolt/Physics/Collision/CollisionCollectors.cs
+++ Jolt/Physics/Collision/CollisionCollectors.cs
@@ -4,59 +4,44 @@ using Unity.Collections;
 
 namespace Jolt
 {
-    /// <summary>
-    /// A collision query result collector designed for burst compatibility.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
     [BurstCompile]
-    public unsafe struct CollisionCollector<T> : IDisposable where T : unmanaged
+    public unsafe struct RayCastResultCollector : IDisposable
     {
         /// <summary>
         /// The collection to receive the results of a collision query.
         /// </summary>
-        public NativeList<T> Results { get; private set; }
+        public NativeList<RayCastResult> Results { get; private set; }
 
-        public CollisionCollector(int capacity, Allocator allocator)
+        internal readonly static FunctionPointer<JPH_CastRayResultCallback> AddResultFuncPointer;
+        internal readonly static FunctionPointer<JPH_CastRayCollector> AddResultWithEarlyOutUpdateFuncPointer;
+
+        static RayCastResultCollector()
         {
-            Results = new NativeList<T>(capacity, allocator);
+            AddResultFuncPointer = BurstCompiler.CompileFunctionPointer<JPH_CastRayResultCallback>(AddResult);
+            AddResultWithEarlyOutUpdateFuncPointer = BurstCompiler.CompileFunctionPointer<JPH_CastRayCollector>(AddResultWithEarlyOutUpdate);
         }
 
-        // TODO : Find better naming for methods and parameters
-
-        [BurstCompile]
-        public static void AddResult(void* context, ref T result)
+        public RayCastResultCollector(int initialCapacity, Allocator allocator)
         {
-            var collectorInstancePtr = (CollisionCollector<T>*)context;
-            var collectorInstance = *collectorInstancePtr;
-
-            collectorInstance.AddResult(ref result);
-        }
-
-        [BurstCompile]
-        public static float AddResultWithEarlyOutUpdate(void* context, ref T result)
-        {
-            var collectorInstancePtr = (CollisionCollector<T>*)context;
-            var collectorInstance = *collectorInstancePtr;
-
-            return collectorInstance.AddResultWithEarlyOutUpdate(ref result);
+            Results = new NativeList<RayCastResult>(initialCapacity, allocator);
         }
 
         [BurstCompile]
-        public static void AddResult2(void* context, T* result)
+        public static void AddResult(void* context, RayCastResult* result)
         {
-            var collectorInstancePtr = (CollisionCollector<T>*)context;
+            var collectorInstancePtr = (RayCastResultCollector*)context;
             var collectorInstance = *collectorInstancePtr;
 
-            collectorInstance.AddResult2(result);
+            collectorInstance.AddResult(result);
         }
 
         [BurstCompile]
-        public static float AddResultWithEarlyOutUpdate2(void* context, T* result)
+        public static float AddResultWithEarlyOutUpdate(void* context, RayCastResult* result)
         {
-            var collectorInstancePtr = (CollisionCollector<T>*)context;
+            var collectorInstancePtr = (RayCastResultCollector*)context;
             var collectorInstance = *collectorInstancePtr;
 
-            return collectorInstance.AddResultWithEarlyOutUpdate2(result);
+            return collectorInstance.AddResultWithEarlyOutUpdate(result);
         }
 
         public void Dispose()
@@ -69,24 +54,201 @@ namespace Jolt
             Results.Clear();
         }
 
-        public void AddResult(ref T result)
-        {
-            Results.Add(result);
-        }
-
-        public float AddResultWithEarlyOutUpdate(ref T result)
-        {
-            Results.Add(result);
-
-            return 1f;
-        }
-
-        public void AddResult2(T* result)
+        public void AddResult(RayCastResult* result)
         {
             Results.Add(*result);
         }
 
-        public float AddResultWithEarlyOutUpdate2(T* result)
+        public float AddResultWithEarlyOutUpdate(RayCastResult* result)
+        {
+            Results.Add(*result);
+
+            return 1f;
+        }
+    }
+
+    [BurstCompile]
+    public unsafe struct ShapeCastResultCollector: IDisposable
+    {
+        /// <summary>
+        /// The collection to receive the results of a collision query.
+        /// </summary>
+        public NativeList<ShapeCastResult> Results { get; private set; }
+
+        internal static FunctionPointer<JPH_CastShapeResultCallback> AddResultFuncPointer;
+        internal static FunctionPointer<JPH_CastShapeCollector> AddResultWithEarlyOutUpdateFuncPointer;
+
+        static ShapeCastResultCollector()
+        {
+            AddResultFuncPointer = BurstCompiler.CompileFunctionPointer<JPH_CastShapeResultCallback>(AddResult);
+            AddResultWithEarlyOutUpdateFuncPointer = BurstCompiler.CompileFunctionPointer<JPH_CastShapeCollector>(AddResultWithEarlyOutUpdate);
+        }
+
+        public ShapeCastResultCollector(int initialCapacity, Allocator allocator)
+        {
+            Results = new NativeList<ShapeCastResult>(initialCapacity, allocator);
+        }
+
+        [BurstCompile]
+        public static void AddResult(void* context, ShapeCastResult* result)
+        {
+            var collectorInstancePtr = (ShapeCastResultCollector*)context;
+            var collectorInstance = *collectorInstancePtr;
+
+            collectorInstance.AddResult(result);
+        }
+
+        [BurstCompile]
+        public static float AddResultWithEarlyOutUpdate(void* context, ShapeCastResult* result)
+        {
+            var collectorInstancePtr = (ShapeCastResultCollector*)context;
+            var collectorInstance = *collectorInstancePtr;
+
+            return collectorInstance.AddResultWithEarlyOutUpdate(result);
+        }
+
+        public void Dispose()
+        {
+            Results.Dispose();
+        }
+
+        public void Clear()
+        {
+            Results.Clear();
+        }
+
+        public void AddResult(ShapeCastResult* result)
+        {
+            Results.Add(*result);
+        }
+
+        public float AddResultWithEarlyOutUpdate(ShapeCastResult* result)
+        {
+            Results.Add(*result);
+
+            return 1f;
+        }
+    }
+
+    [BurstCompile]
+    public unsafe struct CollidePointResultCollector : IDisposable
+    {
+        /// <summary>
+        /// The collection to receive the results of a collision query.
+        /// </summary>
+        public NativeList<CollidePointResult> Results { get; private set; }
+
+        internal readonly static FunctionPointer<JPH_CollidePointResultCallback> AddResultFuncPointer;
+        internal readonly static FunctionPointer<JPH_CollidePointCollector> AddResultWithEarlyOutUpdateFuncPointer;
+
+        static CollidePointResultCollector()
+        {
+            AddResultFuncPointer = BurstCompiler.CompileFunctionPointer<JPH_CollidePointResultCallback>(AddResult);
+            AddResultWithEarlyOutUpdateFuncPointer = BurstCompiler.CompileFunctionPointer<JPH_CollidePointCollector>(AddResultWithEarlyOutUpdate);
+        }
+
+        public CollidePointResultCollector(int initialCapacity, Allocator allocator)
+        {
+            Results = new NativeList<CollidePointResult>(initialCapacity, allocator);
+        }
+
+        [BurstCompile]
+        public static void AddResult(void* context, CollidePointResult* result)
+        {
+            var collectorInstancePtr = (CollidePointResultCollector*)context;
+            var collectorInstance = *collectorInstancePtr;
+
+            collectorInstance.AddResult(result);
+        }
+
+        [BurstCompile]
+        public static float AddResultWithEarlyOutUpdate(void* context, CollidePointResult* result)
+        {
+            var collectorInstancePtr = (CollidePointResultCollector*)context;
+            var collectorInstance = *collectorInstancePtr;
+
+            return collectorInstance.AddResultWithEarlyOutUpdate(result);
+        }
+
+        public void Dispose()
+        {
+            Results.Dispose();
+        }
+
+        public void Clear()
+        {
+            Results.Clear();
+        }
+
+        public void AddResult(CollidePointResult* result)
+        {
+            Results.Add(*result);
+        }
+
+        public float AddResultWithEarlyOutUpdate(CollidePointResult* result)
+        {
+            Results.Add(*result);
+
+            return 1f;
+        }
+    }
+
+    [BurstCompile]
+    public unsafe struct CollideShapeResultCollector : IDisposable
+    {
+        /// <summary>
+        /// The collection to receive the results of a collision query.
+        /// </summary>
+        public NativeList<CollideShapeResult> Results { get; private set; }
+
+        internal readonly static FunctionPointer<JPH_CollideShapeResultCallback> AddResultFuncPointer;
+        internal readonly static FunctionPointer<JPH_CollideShapeCollector> AddResultWithEarlyOutUpdateFuncPointer;
+
+        static CollideShapeResultCollector()
+        {
+            AddResultFuncPointer = BurstCompiler.CompileFunctionPointer<JPH_CollideShapeResultCallback>(AddResult);
+            AddResultWithEarlyOutUpdateFuncPointer = BurstCompiler.CompileFunctionPointer<JPH_CollideShapeCollector>(AddResultWithEarlyOutUpdate);
+        }
+
+        public CollideShapeResultCollector(int initialCapacity, Allocator allocator)
+        {
+            Results = new NativeList<CollideShapeResult>(initialCapacity, allocator);
+        }
+
+        [BurstCompile]
+        public static void AddResult(void* context, CollideShapeResult* result)
+        {
+            var collectorInstancePtr = (CollideShapeResultCollector*)context;
+            var collectorInstance = *collectorInstancePtr;
+
+            collectorInstance.AddResult(result);
+        }
+
+        [BurstCompile]
+        public static float AddResultWithEarlyOutUpdate(void* context, CollideShapeResult* result)
+        {
+            var collectorInstancePtr = (CollideShapeResultCollector*)context;
+            var collectorInstance = *collectorInstancePtr;
+
+            return collectorInstance.AddResultWithEarlyOutUpdate(result);
+        }
+
+        public void Dispose()
+        {
+            Results.Dispose();
+        }
+
+        public void Clear()
+        {
+            Results.Clear();
+        }
+
+        public void AddResult(CollideShapeResult* result)
+        {
+            Results.Add(*result);
+        }
+
+        public float AddResultWithEarlyOutUpdate(CollideShapeResult* result)
         {
             Results.Add(*result);
 
