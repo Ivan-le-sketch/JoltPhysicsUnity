8bb8b18c109128bf4d5a074c16478ce8b6519994
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2025-01-02 11:31:52 +0100

    Update BodyFilter to support Burst

--- Jolt/Physics/Collision/BodyFilter.cs
+++ Jolt/Physics/Collision/BodyFilter.cs
@@ -1,78 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Unity.Burst;
+using Unity.Collections;
 
 namespace Jolt
 {
     /// <summary>
     /// Filter defining bodies to ignore in a collision query.
     /// </summary>
-    public unsafe class BodyFilter : IDisposable
+    [BurstCompile]
+    public unsafe struct BodyFilter : IDisposable
     {
+        /// <summary>
+        /// Enumerates the modes of filtering for <see cref="BodyFilter"/>.
+        /// </summary>
         public enum FilterMode
         {
-            CollideWithSelection,
             IgnoreSelection,
+            CollideWithSelection,
         }
 
+        /// <summary>
+        /// A handle to a native body filter.
+        /// </summary>
         internal NativeHandle<JPH_BodyFilter> Handle;
 
+        /// <summary>
+        /// A pointer to a <see cref="BodyFilter"/> instance allocated on the unmanaged heap.
+        /// This instance is safe to use in interoperation context as it is not managed.
+        /// Every modifications affect this instance. 
+        /// </summary>
+        internal IntPtr UnmanagedPointer;
+
+        /// <summary>
+        /// The filtering mode.
+        /// </summary>
         private FilterMode mode;
-        private HashSet<BodyID> bodyIDSelection = new HashSet<BodyID>();
+        /// <summary>
+        /// The <see cref="BodyID"/> selection to either ignore or collide with depending on <see cref="mode"/>.
+        /// </summary>
+        private NativeHashSet<BodyID> bodyIDSelection;
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate bool ShouldCollide(IntPtr context, BodyID bodyID);
+        private delegate bool ShouldCollideSignature(void* context, BodyID bodyID);
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate bool ShouldCollideLocked(IntPtr context, JPH_Body* body);
+        private delegate bool ShouldCollideLockedSignature(void* context, JPH_Body* body);
 
-        // Keep a reference to the delegate to prevent garbage collection
-        internal ShouldCollide shouldCollideDelegate;
-        // Keep a reference to the delegate to prevent garbage collection
-        internal ShouldCollideLocked shouldCollideLockedDelegate;
+        private static readonly FunctionPointer<ShouldCollideSignature> shouldCollideFunctionPointer;
+        private static readonly FunctionPointer<ShouldCollideLockedSignature> shouldCollideLockedFunctionPointer;
 
-        public BodyFilter()
+        static BodyFilter()
         {
-            CreateNativeHandle();
+            shouldCollideFunctionPointer = BurstCompiler.CompileFunctionPointer<ShouldCollideSignature>(ShouldCollide);
+            shouldCollideLockedFunctionPointer = BurstCompiler.CompileFunctionPointer<ShouldCollideLockedSignature>(ShouldCollideLocked);
         }
 
         public BodyFilter(IEnumerable<BodyID> bodyIDs, FilterMode filterMode)
         {
-            CreateNativeHandle();
+            Handle = default;
+            UnmanagedPointer = default;
 
+            bodyIDSelection = new NativeHashSet<BodyID>(8, Allocator.Persistent);
             foreach (BodyID bodyID in bodyIDs)
             {
-                AddBodyIDToSelection(bodyID);
+                bodyIDSelection.Add(bodyID);
             }
 
             mode = filterMode;
         }
 
-        public void Dispose()
+        /// <summary>
+        /// Initializes a new <see cref="BodyFilter"/> instance on the unmanaged heap.
+        /// </summary>
+        /// <param name="bodyIDs"></param>
+        /// <param name="filterMode"></param>
+        /// <returns></returns>
+        public static BodyFilter Create(IEnumerable<BodyID> bodyIDs, FilterMode filterMode)
         {
-            Bindings.JPH_BodyFilter_Destroy(Handle);
+            var unmanagedPointer = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(BodyFilter)));
+            BodyFilter filter = new BodyFilter(bodyIDs, filterMode);
+            Marshal.StructureToPtr(filter, unmanagedPointer, false);
+
+            filter.UnmanagedPointer = unmanagedPointer;
+
+            var ptr = (BodyFilter*)unmanagedPointer.ToPointer();
+
+            var procs = new JPH_BodyFilter_Procs
+            {
+                ShouldCollide = shouldCollideFunctionPointer.Value,
+                ShouldCollideLocked = shouldCollideLockedFunctionPointer.Value,
+            };
+
+            filter.Handle = Bindings.JPH_BodyFilter_Create(procs, ptr);
+
+            return filter;
         }
 
-        public void SetFilterMode(FilterMode filterMode)
+        /// <summary>
+        /// <para>Static callback method fo Jolt physics engine to call.</para>
+        /// Returns if the body identified by <paramref name="bodyID"/> should collide.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="bodyID"></param>
+        /// <returns></returns>
+        [BurstCompile]
+        internal static bool ShouldCollide(void* context, BodyID bodyID)
         {
-            mode = filterMode;
+            var implPtr = (BodyFilter*)context;
+            var impl = *implPtr;
+
+            return impl.ShouldCollide(bodyID);
         }
 
-        public void AddBodyIDToSelection(BodyID bodyID)
+        /// <summary>
+        /// <para>Static callback method fo Jolt physics engine to call.</para>
+        /// Returns if <paramref name="body"/> should collide.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        [BurstCompile]
+        internal static bool ShouldCollideLocked(void* context, JPH_Body* body)
         {
-            bodyIDSelection.Add(bodyID);
+            var bodyHandle = new Body(new NativeHandle<JPH_Body>(body));
+
+            bool result = true;
+
+            var implPtr = (BodyFilter*)context;
+            var impl = *implPtr;
+
+            result = impl.ShouldCollideLocked(bodyHandle);
+
+            bodyHandle.Handle.Dispose();
+
+            return result;
         }
 
-        public void RemoveBodyIDFromSelection(BodyID bodyID)
-        {
-            bodyIDSelection.Remove(bodyID);
-        }
-
-        public void ClearBodyIDSelection()
-        {
-            bodyIDSelection.Clear();
-        }
-
-        internal bool ShouldCollideCallback(IntPtr context, BodyID bodyID)
+        /// <summary>
+        /// Returns if the body identified by <paramref name="bodyID"/> should collide.
+        /// </summary>
+        /// <param name="bodyID"></param>
+        /// <returns></returns>
+        public bool ShouldCollide(BodyID bodyID)
         {
             if (mode == FilterMode.CollideWithSelection)
             {
@@ -84,38 +154,94 @@ namespace Jolt
             }
         }
 
-        internal bool ShouldCollideLockedCallback(IntPtr context, JPH_Body* body)
+        /// <summary>
+        /// Returns if <paramref name="body"/> should collide.
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public bool ShouldCollideLocked(Body body)
         {
-            var bodyHandle = new Body(new NativeHandle<JPH_Body>(body));
-
-            bool result = true;
-
             if (mode == FilterMode.CollideWithSelection)
             {
-                result = bodyIDSelection.Contains(bodyHandle.GetID());
+                return bodyIDSelection.Contains(body.GetID());
             }
             else
             {
-                result = !bodyIDSelection.Contains(bodyHandle.GetID());
+                return !bodyIDSelection.Contains(body.GetID());
             }
-
-            bodyHandle.Handle.Dispose();
-
-            return result;
         }
 
-        private void CreateNativeHandle()
+        /// <summary>
+        /// Sets the <see cref="FilterMode"/> to <paramref name="filterMode"/>.
+        /// </summary>
+        /// <param name="filterMode"></param>
+        public void SetFilterMode(FilterMode filterMode)
         {
-            shouldCollideDelegate = ShouldCollideCallback;
-            shouldCollideLockedDelegate = ShouldCollideLockedCallback;
+            var ptr = UnmanagedPointer.ToPointer();
 
-            var procs = new JPH_BodyFilter_Procs
+            if (ptr == null)
             {
-                ShouldCollide = Marshal.GetFunctionPointerForDelegate(shouldCollideDelegate),
-                ShouldCollideLocked = Marshal.GetFunctionPointerForDelegate(shouldCollideLockedDelegate),
-            };
+                throw new Exception("Unvalid pointer.");
+            }
 
-            Handle = Bindings.JPH_BodyFilter_Create(procs);
+            var unmanagedInstance = *(BodyFilter*)ptr;
+            unmanagedInstance.mode = filterMode;
+        }
+
+        /// <summary>
+        /// Adds a new <see cref="BodyID"/> to the ignored/collidable selection.
+        /// </summary>
+        /// <param name="bodyID"></param>
+        public void AddBodyIDToSelection(BodyID bodyID)
+        {
+            var ptr = UnmanagedPointer.ToPointer();
+
+            if (ptr == null)
+            {
+                throw new Exception("Unvalid pointer.");
+            }
+
+            var unmanagedInstance = *(BodyFilter*)ptr;
+            unmanagedInstance.bodyIDSelection.Add(bodyID);
+        }
+
+        /// <summary>
+        /// Tries to remove <paramref name="bodyID"/> from the ignored/collidable selection.
+        /// </summary>
+        /// <param name="bodyID"></param>
+        public void RemoveBodyIDFromSelection(BodyID bodyID)
+        {
+            var ptr = UnmanagedPointer.ToPointer();
+
+            if (ptr == null)
+            {
+                throw new Exception("Unvalid pointer.");
+            }
+
+            var unmanagedInstance = *(BodyFilter*)ptr;
+            unmanagedInstance.bodyIDSelection.Remove(bodyID);
+        }
+
+        /// <summary>
+        /// Clears the selection.
+        /// </summary>
+        public void ClearBodyIDSelection()
+        {
+            var ptr = UnmanagedPointer.ToPointer();
+
+            if (ptr == null)
+            {
+                throw new Exception("Unvalid pointer.");
+            }
+
+            var unmanagedInstance = *(BodyFilter*)ptr;
+            unmanagedInstance.bodyIDSelection.Clear();
+        }
+
+        public void Dispose()
+        {
+            Bindings.JPH_BodyFilter_Destroy(Handle);
+            Marshal.FreeHGlobal(UnmanagedPointer);
         }
     }
 }
