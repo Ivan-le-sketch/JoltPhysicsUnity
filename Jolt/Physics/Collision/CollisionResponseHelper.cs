8168e8ca0e1f86aadf080854a541f84ecfd009ee
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2025-02-02 21:37:17 +0100

    Add CollisionResponseHelper class
    Creates a class to hold the bindings for static method EstimateCollisionResponse.

---
+++ Jolt/Physics/Collision/CollisionResponseHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Jolt
+{
+    public static class CollisionResponseHelper
+    {
+        public static CollisionEstimationResult EstimateCollisionResponse(
+            Body body1, Body body2,
+            ContactManifold manifold,
+            float combinedFriction, float combinedRestitution,
+            float minVelocityForRestitution,
+            uint numIterations
+            )
+        {
+            return Bindings.JPH_EstimateCollisionResponse(
+                body1.Handle, body2.Handle, 
+                manifold.Handle, 
+                combinedFriction, combinedRestitution, 
+                minVelocityForRestitution, 
+                numIterations);
+        }
+    }
+}
