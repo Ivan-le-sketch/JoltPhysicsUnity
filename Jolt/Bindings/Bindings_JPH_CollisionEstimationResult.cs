057a5632e3d16955eab4fbba55d625ade2d918fd
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2025-02-08 17:24:09 +0100

    Update Bindings_JPH_CollisionEstimationResult.cs
    Reflects changes in joltc.

--- Jolt/Bindings/Bindings_JPH_CollisionEstimationResult.cs
+++ Jolt/Bindings/Bindings_JPH_CollisionEstimationResult.cs
@@ -23,32 +23,50 @@ namespace Jolt
 
         public static float3 JPH_CollisionEstimationResult_GetLinearVelocity1(NativeHandle<JPH_CollisionEstimationResult> collisionEstimationResult)
         {
-            return UnsafeBindings.JPH_CollisionEstimationResult_GetLinearVelocity1(collisionEstimationResult);
+            float3 linearVelocity1;
+            UnsafeBindings.JPH_CollisionEstimationResult_GetLinearVelocity1(collisionEstimationResult, &linearVelocity1);
+
+            return linearVelocity1;
         }
 
         public static float3 JPH_CollisionEstimationResult_GetLinearVelocity2(NativeHandle<JPH_CollisionEstimationResult> collisionEstimationResult)
         {
-            return UnsafeBindings.JPH_CollisionEstimationResult_GetLinearVelocity2(collisionEstimationResult);
+            float3 linearVelocity2;
+            UnsafeBindings.JPH_CollisionEstimationResult_GetLinearVelocity2(collisionEstimationResult, &linearVelocity2);
+
+            return linearVelocity2;
         }
 
         public static float3 JPH_CollisionEstimationResult_GetAngularVelocity1(NativeHandle<JPH_CollisionEstimationResult> collisionEstimationResult)
         {
-            return UnsafeBindings.JPH_CollisionEstimationResult_GetAngularVelocity1(collisionEstimationResult);
+            float3 angularVelocity1;
+            UnsafeBindings.JPH_CollisionEstimationResult_GetAngularVelocity1(collisionEstimationResult, &angularVelocity1);
+
+            return angularVelocity1;
         }
 
         public static float3 JPH_CollisionEstimationResult_GetAngularVelocity2(NativeHandle<JPH_CollisionEstimationResult> collisionEstimationResult)
         {
-            return UnsafeBindings.JPH_CollisionEstimationResult_GetAngularVelocity2(collisionEstimationResult);
+            float3 angularVelocity2;
+            UnsafeBindings.JPH_CollisionEstimationResult_GetAngularVelocity2(collisionEstimationResult, &angularVelocity2);
+
+            return angularVelocity2;
         }
 
         public static float3 JPH_CollisionEstimationResult_GetTangent1(NativeHandle<JPH_CollisionEstimationResult> collisionEstimationResult)
         {
-            return UnsafeBindings.JPH_CollisionEstimationResult_GetTangent1(collisionEstimationResult);
+            float3 tangent1;
+            UnsafeBindings.JPH_CollisionEstimationResult_GetTangent1(collisionEstimationResult, &tangent1);
+
+            return tangent1;
         }
 
         public static float3 JPH_CollisionEstimationResult_GetTangent2(NativeHandle<JPH_CollisionEstimationResult> collisionEstimationResult)
         {
-            return UnsafeBindings.JPH_CollisionEstimationResult_GetTangent2(collisionEstimationResult);
+            float3 tangent2;
+            UnsafeBindings.JPH_CollisionEstimationResult_GetTangent2(collisionEstimationResult, &tangent2);
+
+            return tangent2;
         }
 
         public static uint JPH_CollisionEstimationResult_GetImpulsesCount(NativeHandle<JPH_CollisionEstimationResult> collisionEstimationResult)
