d78116b531b7c5cf734af6a17f880ccaf4bf8a97
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2025-03-03 11:18:11 +0100

    Update CollideShapeResult.cs

--- Jolt/Physics/CollideShapeResult.cs
+++ Jolt/Physics/CollideShapeResult.cs
@@ -4,22 +4,20 @@ using Unity.Mathematics;
 
 namespace Jolt
 {
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct CollideShapeResult
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public unsafe struct CollideShapeResult
     {
-        public float3 ContactPointOn1;
-        
-        public float3 ContactPointOn2;
-        
-        public float3 PenetrationAxis;
-        
+        public float3 ContactPointOn1; 
+        public float3 ContactPointOn2;  
+        public float3 PenetrationAxis; 
         public float PenetrationDepth;
-
-        public SubShapeID SubShapeID1;
-        
+        public SubShapeID SubShapeID1;   
         public SubShapeID SubShapeID2;
-
         public BodyID BodyID2;
+        public uint shape1FaceCount;
+        public float3* shape1Faces;
+        public uint shape2FaceCount;
+        public float3* shape2Faces;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public float GetEarlyOutFraction()
