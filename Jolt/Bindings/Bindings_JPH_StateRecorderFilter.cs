8a621f7fd28bce9f1298f5ec50f968437e6b116a
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2025-06-06 14:06:53 +0200

    Rework StateRecorderFilter

--- Jolt/Bindings/Bindings_JPH_StateRecorderFilter.cs
+++ Jolt/Bindings/Bindings_JPH_StateRecorderFilter.cs
@@ -1,21 +1,121 @@
-﻿using System;
+﻿using AOT;
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
 
 namespace Jolt
 {
     internal static unsafe partial class Bindings
     {
-        public static NativeHandle<JPH_StateRecorderFilter> JPH_StateRecorderFilter_Create(JPH_StateRecorderFilter_Procs procs, IntPtr userData)
+        public static NativeHandle<JPH_StateRecorderFilter> JPH_StateRecorderFilter_Create(IStateRecorderFilter filter)
         {
-            return CreateHandle(UnsafeBindings.JPH_StateRecorderFilter_Create(procs, (void*) userData));
+            var gch = GCHandle.Alloc(filter);
+            var ptr = GCHandle.ToIntPtr(gch);
+
+            fixed (JPH_StateRecorderFilter_Procs* procsPtr = &UnsafeStateRecorderFilterProcs)
+            {
+                var handle = CreateHandle(UnsafeBindings.JPH_StateRecorderFilter_Create(UnsafeStateRecorderFilterProcs, (void*)ptr)); // TODO
+
+                ManagedReference.Add(handle, gch);
+
+                return handle;
+            }
         }
 
         public static void JPH_StateRecorderFilter_Destroy(NativeHandle<JPH_StateRecorderFilter> filter)
         {
             if (filter.HasUser()) return;
 
+            if (ManagedReference.Remove(filter, out var gch))
+            {
+                gch.Free();
+            }
+            else
+            {
+                Debug.LogError("Missing GCHandle for managed state recorder filter!");
+            }
+
             UnsafeBindings.JPH_StateRecorderFilter_Destroy(filter);
 
             filter.Dispose();
+        }
+
+        /// <summary>
+        /// Static procs for marshalling; the lookup from static to instance context happens in each method.
+        /// </summary>
+        private static readonly JPH_StateRecorderFilter_Procs UnsafeStateRecorderFilterProcs = new()
+        {
+            ShouldSaveBody = GetDelegatePointer((UnsafeShouldSaveBody)UnsafeShouldSaveBodyCallback),
+            ShouldSaveConstraint = GetDelegatePointer((UnsafeShouldSaveConstraint)UnsafeShouldSaveConstraintCallback),
+            ShouldSaveContact = GetDelegatePointer((UnsafeShouldSaveContact)UnsafeShouldSaveContactCallback),
+            ShouldRestoreContact = GetDelegatePointer((UnsafeShouldRestoreContact)UnsafeShouldRestoreContactCallback),
+        };
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate bool UnsafeShouldSaveBody(IntPtr userData, JPH_Body* body);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate bool UnsafeShouldSaveConstraint(IntPtr userData, JPH_Constraint* constraint);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate bool UnsafeShouldSaveContact(IntPtr userData, BodyID bodyID1, BodyID bodyID2);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate bool UnsafeShouldRestoreContact(IntPtr userData, BodyID bodyID1, BodyID bodyID2);
+
+        [MonoPInvokeCallback(typeof(UnsafeShouldSaveBody))]
+        private static bool UnsafeShouldSaveBodyCallback(IntPtr userData, JPH_Body* body)
+        {
+            try
+            {
+                var b = new Body(new NativeHandle<JPH_Body>(body));
+                return ManagedReference.Deref<IStateRecorderFilter>(userData).ShouldSaveBody(b);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                return true;
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(UnsafeShouldSaveConstraint))]
+        private static bool UnsafeShouldSaveConstraintCallback(IntPtr userData, JPH_Constraint* constraint)
+        {
+            try
+            {
+                var c = new Constraint(new NativeHandle<JPH_Constraint>(constraint));
+                return ManagedReference.Deref<IStateRecorderFilter>(userData).ShouldSaveConstraint(c);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                return true;
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(UnsafeShouldSaveContact))]
+        private static bool UnsafeShouldSaveContactCallback(IntPtr userData, BodyID bodyID1, BodyID bodyID2)
+        {
+            try
+            {
+                return ManagedReference.Deref<IStateRecorderFilter>(userData).ShouldSaveContact(bodyID1, bodyID2);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                return true;
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(UnsafeShouldRestoreContact))]
+        private static bool UnsafeShouldRestoreContactCallback(IntPtr userData, BodyID bodyID1, BodyID bodyID2)
+        {
+            try
+            {
+                return ManagedReference.Deref<IStateRecorderFilter>(userData).ShouldRestoreContact(bodyID1, bodyID2);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                return true;
+            }
         }
     }
 }
--- Jolt/Physics/StateRecorderFilter.cs
+++ Jolt/Physics/StateRecorderFilter.cs
@@ -3,102 +3,26 @@ using System.Runtime.InteropServices;
 
 namespace Jolt
 {
-    public unsafe class StateRecorderFilter
+    public interface IStateRecorderFilter
+    {
+        public bool ShouldSaveBody(Body body);
+        public bool ShouldSaveConstraint(Constraint constraint);
+        public bool ShouldSaveContact(BodyID bodyID1, BodyID bodyID2);
+        public bool ShouldRestoreContact(BodyID bodyID1, BodyID bodyID2);
+    }
+
+    public partial struct StateRecorderFilter
     {
         internal NativeHandle<JPH_StateRecorderFilter> Handle;
 
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate bool ShouldSaveBody(IntPtr userData, JPH_Body* body);
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate bool ShouldSaveConstraint(IntPtr userData, JPH_Constraint* constraint);
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate bool ShouldSaveContact(IntPtr userData, BodyID bodyID1, BodyID bodyID2);
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate bool ShouldRestoreContact(IntPtr userData, BodyID bodyID1, BodyID bodyID2);
-
-        private ShouldSaveBody shouldSaveBodyDelegate;
-        private ShouldSaveConstraint shouldSaveConstraintDelegate;
-        private ShouldSaveContact shouldSaveContactDelegate;
-        private ShouldSaveContact shouldRestoreContactDelegate;
-
-        public StateRecorderFilter()
+        public static StateRecorderFilter Create(IStateRecorderFilter implementation)
         {
-            CreateNativeHandle();
+            return new StateRecorderFilter { Handle = Bindings.JPH_StateRecorderFilter_Create(implementation) };
         }
 
         public void Destroy()
         {
             Bindings.JPH_StateRecorderFilter_Destroy(Handle);
-        }
-
-        public virtual bool ShouldSaveBodyCallbackImplementation(Body body)
-        {
-            return true;
-        }
-
-        public virtual bool ShouldSaveConstraintCallbackImplementation(Constraint constraint)
-        {
-            return true;
-        }
-
-        public virtual bool ShouldSaveContactCallbackImplementation(BodyID bodyID1, BodyID bodyID2)
-        {
-            return true;
-        }
-
-        public virtual bool ShouldRestoreContactCallbackImplementation(BodyID bodyID1, BodyID bodyID2)
-        {
-            return true;
-        }
-
-        internal bool ShouldSaveBodyCallback(IntPtr userData, JPH_Body* unsafeBody)
-        {
-            var body = new Body(new NativeHandle<JPH_Body>(unsafeBody));
-
-            bool result = ShouldSaveBodyCallbackImplementation(body);
-
-            body.Handle.Dispose();
-
-            return result;
-        }
-
-        internal bool ShouldSaveConstraintCallback(IntPtr userData, JPH_Constraint* unsafeConstraint)
-        {
-            var constraint = new Constraint(new NativeHandle<JPH_Constraint>(unsafeConstraint));
-
-            bool result = ShouldSaveConstraintCallbackImplementation(constraint);
-
-            constraint.Handle.Dispose();
-
-            return result;
-        }
-
-        internal bool ShouldSaveContactCallback(IntPtr userData, BodyID bodyID1, BodyID bodyID2)
-        {
-            return ShouldSaveContactCallbackImplementation(bodyID1, bodyID2);
-        }
-
-        internal bool ShouldRestoreContactCallback(IntPtr userData, BodyID bodyID1, BodyID bodyID2)
-        {
-            return ShouldRestoreContactCallbackImplementation(bodyID1, bodyID2);
-        }
-
-        protected void CreateNativeHandle()
-        {
-            shouldSaveBodyDelegate = ShouldSaveBodyCallback;
-            shouldSaveConstraintDelegate = ShouldSaveConstraintCallback;
-            shouldSaveContactDelegate = ShouldSaveContactCallback;
-            shouldRestoreContactDelegate = ShouldRestoreContactCallback;
-
-            JPH_StateRecorderFilter_Procs procs = new JPH_StateRecorderFilter_Procs
-            {
-                ShouldSaveBody = Marshal.GetFunctionPointerForDelegate(shouldSaveBodyDelegate),
-                ShouldSaveConstraint = Marshal.GetFunctionPointerForDelegate(shouldSaveConstraintDelegate),
-                ShouldSaveContact = Marshal.GetFunctionPointerForDelegate(shouldSaveContactDelegate),
-                ShouldRestoreContact = Marshal.GetFunctionPointerForDelegate(shouldRestoreContactDelegate),
-            };
-
-            Handle = Bindings.JPH_StateRecorderFilter_Create(procs, IntPtr.Zero);
         }
     }
 }
